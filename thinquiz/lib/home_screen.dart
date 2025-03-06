0674ec679ed07cd19e793295400acb5d7130284f
837100 <shtjdrud0830@naver.com>
2025-03-06 22:59:00 +0900

    feat: 게임 및 퀴즈 데이터 모델 추가 및 홈 화면 업데이트

--- thinquiz/lib/home_screen.dart
+++ thinquiz/lib/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'model/game.dart';
+import 'model/quiz.dart';
+
 class MainScreen extends StatelessWidget {
-  final Future<List<dynamic>> data;
-  const MainScreen({super.key, required this.data});
+  final Future<List<Game>> gameData;
+  final Future<List<Quiz>> quizData;
+  const MainScreen({super.key, required this.gameData, required this.quizData});
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +19,23 @@ class MainScreen extends StatelessWidget {
               SizedBox(
                 height: 20,
               ),
-              Center(
-                child: Container(
-                  padding: EdgeInsets.all(16), // 텍스트 주위에 패딩 추가
-                  decoration: BoxDecoration(
-                    color: Color(0xFFD6D5C9), //
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-
-                  child: Text('''
+              FutureBuilder<List<Game>>(
+                future: gameData,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('오류 발생: ${snapshot.error}');
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(16), // 텍스트 주위에 패딩 추가
+                          decoration: BoxDecoration(
+                            color: Color(0xFFD6D5C9), //
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Text('''
 🔍 당신의 지혜와 운을 시험해보세요!
                               
 이 앱은 단순한 퀴즈 게임이 아닙니다.
@@ -33,18 +45,75 @@ class MainScreen extends StatelessWidget {
 게임 중 뜻밖의 기회(?)가 찾아올지도 모릅니다.
 마지막까지 도전할 준비가 되었나요? 🎲✨
                 ''', textAlign: TextAlign.center),
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF902923),
-                    foregroundColor: Color(0xFF0A100D),
-                  ),
-                  child: Text('시작 하기')),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF902923),
+                            foregroundColor: Color(0xFF0A100D),
+                          ),
+                          child: Text('시작 하기'),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(16), // 텍스트 주위에 패딩 추가
+                          decoration: BoxDecoration(
+                            color: Color(0xFFD6D5C9), //
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Text(
+                              '진행 상황 ${snapshot.data!.fold(0, (sum, game) => sum + game.totalPoint)}점'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF902923),
+                            foregroundColor: Color(0xFF0A100D),
+                          ),
+                          child: Text('이어 하기'),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color(0xFF003049),
+                                  foregroundColor: Color(0xFF0A100D),
+                                ),
+                                child: Text('맞힌 문제 보기'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color(0xFF902923),
+                                  foregroundColor: Color(0xFF0A100D),
+                                ),
+                                child: Text('진행 사항 초기화'),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    );
+                  }
+                },
+              )
             ],
           ),
         ),
--- thinquiz/lib/main.dart
+++ thinquiz/lib/main.dart
@@ -2,13 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:thinquiz/model/quiz.dart';
 
 import 'home_screen.dart';
+import 'model/game.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<Quiz> _testData = [
+  final List<Game> _gameData = [
+    //
+    Game(
+      hintCount: 3,
+      quizIndex: 0,
+      totalPoint: 100,
+      quizList: [
+        Quiz(
+          title: '퀴즈 1',
+          point: 10,
+          hint: '힌트 1',
+          content: '내용 1',
+          answer: '정답 1',
+          solution: '해설 1',
+          status: '상태 1',
+          quizImage: '이미지 1',
+        ),
+        Quiz(
+          title: '퀴즈 2',
+          point: 20,
+          hint: '힌트 2',
+          content: '내용 2',
+          answer: '정답 2',
+          solution: '해설 2',
+          status: '상태 2',
+          quizImage: '이미지 2',
+        ),
+        Quiz(
+          title: '퀴즈 3',
+          point: 30,
+          hint: '힌트 3',
+          content: '내용 3',
+          answer: '정답 3',
+          solution: '해설 3',
+          status: '상태 3',
+          quizImage: '이미지 3',
+        ),
+      ],
+    ),
+
+    //
+  ];
+
+  final List<Quiz> _quizData = [
     Quiz(
       title: '퀴즈 1',
       point: 10,
@@ -51,7 +95,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MainScreen(data: Future.value(_testData)),
+      home: MainScreen(
+          gameData: Future.value(_gameData), quizData: Future.value(_quizData)),
     );
   }
 }
